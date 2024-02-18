e6d3fa861b4ea5e7ed4061bd04cefcbf8e94fef9
root <root@master.demo.lab>
2024-02-18 15:31:52 +0530

    Job Script

---
+++ myscript1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --job-name=activity1
+#SBATCH --nodes=2
+#SBATCH --partition=hpc
+#SBATCH --error=job.%J.err
+#SBATCH --output=job.%j.out
+python3 /nfs/slurm-rpms/detecting_fake_news-main/detecting_fake_news-main/detecting_fake_news.py
+echo "THIS IS SANDIP"
